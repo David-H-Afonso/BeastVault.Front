55983595aa259e66fdb64f1ad143beeca077e61e
David Hormiga Afonso <davidhormigafonso@gmail.com>
2025-09-09 18:53:16 +0200

    Refactor: Add Pokémon preview section with Charizard default values and sprites

--- src/components/Settings/containers/Settings.tsx
+++ src/components/Settings/containers/Settings.tsx
@@ -5,11 +5,68 @@ import type { SpriteTypeName } from '@/models/enums/SpriteTypes'
 import { CardBackgroundType, CardBackgroundLabels } from '@/models/enums/CardBackgroundTypes'
 import { SPRITE_TYPE_CONFIG } from '@/models/enums/SpriteTypes'
 import { useUISettings } from '@/hooks/useUISettings'
+import { PokemonCard } from '@/components/elements'
+import { getBestSpriteByType } from '@/utils'
+
+const CHARIZARD_DEFAULT_SPRITES = {
+	back_default:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png',
+	back_shiny:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/6.png',
+	default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+	dreamWorld:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg',
+	githubRegular:
+		'https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/regular/charizard.png',
+	githubShiny:
+		'https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen8/shiny/charizard.png',
+	home: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png',
+	homeShiny:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/6.png',
+	official:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png',
+	officialShiny:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/6.png',
+	shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/6.png',
+	showdown:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/6.gif',
+	showdownShiny:
+		'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/6.gif',
+}
+
+const CHARIZARD_DEFAULT_VALUES = {
+	id: 6,
+	speciesId: 6,
+	form: 0,
+	formName: 'Standard',
+	canGigantamax: false,
+	speciesName: 'Charizard',
+	nickname: 'Charizard',
+	level: 100,
+	isShiny: false,
+	ballId: 0,
+	spriteKey: '1025_n_0',
+	originGeneration: 1,
+	capturedGeneration: 9,
+	hasMegaStone: true,
+}
 
 const Settings: React.FC = () => {
 	const { backgroundType, setBackgroundType, spriteType, setSpriteType } = useUISettings()
 
 	const backgroundOptions = Object.values(CardBackgroundType) as CardBackgroundTypeName[]
+
+	const defaultPokemon = [
+		{
+			...CHARIZARD_DEFAULT_VALUES,
+		},
+		{
+			...CHARIZARD_DEFAULT_VALUES,
+			isShiny: true,
+		},
+	]
+	const spriteURL = (isShiny: boolean) =>
+		getBestSpriteByType(CHARIZARD_DEFAULT_SPRITES, spriteType, isShiny) || ''
 
 	return (
 		<div className='settings-page'>
@@ -20,6 +77,23 @@ const Settings: React.FC = () => {
 						Configure your BeastVault experience to your preferences
 					</p>
 				</header>
+
+				{/* Preview Section */}
+				<section className='settings-section' style={{ marginBottom: '20px' }}>
+					<h2 className='section-title'>👁️ Preview</h2>
+					<div className='section-content'>
+						{defaultPokemon.map((pokemon, index) => (
+							<PokemonCard
+								key={index}
+								pokemon={pokemon}
+								sprite={spriteURL(pokemon.isShiny)}
+								onDownload={() => {}}
+								onDelete={() => {}}
+								onManageTags={() => {}}
+							/>
+						))}
+					</div>
+				</section>
 
 				<div className='settings-content'>
 					{/* Display Options */}
